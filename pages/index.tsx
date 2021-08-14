4d7182dd77fea5cdcff2c039bb4b8815e5d0e4ef
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-08-14 20:45:16 +0545

    refactor: removed sass, added chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import React from "react";
+
 import type { NextPage } from "next";
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Head>
+        <title>Password Manager</title>
+      </Head>
+      <Box>Hello, World!</Box>
+    </>
+  );
 };
 
 export default Home;
