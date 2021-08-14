9ba202d87e2802956a90815023f1c38c851f0dd1
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-08-14 22:07:52 +0545

    feature: header complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,56 @@
 import React from "react";
 
+import "focus-visible/dist/focus-visible";
+
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Box } from "@chakra-ui/react";
+
+import {
+  Heading,
+  HStack,
+  LinkOverlay,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+  Link,
+  Icon,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+import { FaGithub } from "react-icons/fa";
 
 const Home: NextPage = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <>
       <Head>
         <title>Password Manager</title>
       </Head>
-      <Box>Hello, World!</Box>
+      <HStack
+        id="header"
+        justifyContent="space-between"
+        paddingY={4}
+        paddingX={{ base: 4, md: 8 }}
+        boxShadow="lg"
+      >
+        <Heading>Password Manager</Heading>
+
+        <HStack spacing={4}>
+          <IconButton
+            aria-label="Switch between dark and light mode"
+            icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+            backgroundColor="transparent"
+            onClick={toggleColorMode}
+          />
+          <Link
+            isExternal
+            cursor="pointer"
+            href="https://github.com/aabishkaryal/password-manager"
+          >
+            <Icon as={FaGithub} />
+          </Link>
+        </HStack>
+      </HStack>
     </>
   );
 };
