873c19fb36e188643ad5f8105235a446c1da6aad
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-11-03 20:46:27 +0545

    bug fix, included ambig character even when isAmbigious was false

--- app/password.ts
+++ app/password.ts
@@ -1,40 +1,46 @@
 import { GetRandomInt } from "@app/utils";
 import {
-	ALPHABETS,
-	NUMBERS,
-	SPECIAL_CHARS,
-	AMBIGIOUS_CHARS,
+    ALPHABETS,
+    NUMBERS,
+    SPECIAL_CHARS,
+    AMBIGIOUS_CHARS,
 } from "@app/constants";
 
 export type GeneratorParams = {
-	numLetters: number;
-	numNumbers?: number;
-	numSpecialChars?: number;
-	isAmbigious?: boolean;
+    numLetters: number;
+    numNumbers: number;
+    numSpecialChars: number;
+    isAmbigious: boolean;
 };
 
 export function GeneratePassword({
-	numLetters = 8,
-	numNumbers = 0,
-	numSpecialChars = 0,
-	isAmbigious = false,
+    numLetters,
+    numNumbers,
+    numSpecialChars,
+    isAmbigious,
 }: GeneratorParams): string {
-	const randomLetters = GetRandomChars(ALPHABETS, numLetters).split("");
-	const randomNumbers = GetRandomChars(NUMBERS, numNumbers).split("");
-	const randomSpecialChars = GetRandomChars(SPECIAL_CHARS + isAmbigious ? AMBIGIOUS_CHARS : "", numSpecialChars).split("");
-	const result = [...randomLetters, ...randomNumbers, ...randomSpecialChars]
-	result.sort(() => 0.5 - Math.random());
-	return result.join("");
+    const randomLetters = GetRandomChars(ALPHABETS, numLetters).split("");
+    const randomNumbers = GetRandomChars(NUMBERS, numNumbers).split("");
+    const specialChars = SPECIAL_CHARS.concat(
+        isAmbigious ? AMBIGIOUS_CHARS : ""
+    );
+    const randomSpecialChars = GetRandomChars(
+        specialChars,
+        numSpecialChars
+    ).split("");
+    const result = [...randomLetters, ...randomNumbers, ...randomSpecialChars];
+    result.sort(() => 0.5 - Math.random());
+    return result.join("");
 }
 
 // Given an string and a number return a new string with n random characters (repition allowed)
 export function GetRandomChars(str: string, n: number): string {
-	const chars = [];
-	
-	for (let i = 0; i < n; i++) {
-		const randomIndex = GetRandomInt(0, str.length);
-		chars.push(str[randomIndex]);
-	}
-	
-	return chars.join("");
+    const chars = [];
+
+    for (let i = 0; i < n; i++) {
+        const randomIndex = GetRandomInt(0, str.length);
+        chars.push(str[randomIndex]);
+    }
+
+    return chars.join("");
 }
