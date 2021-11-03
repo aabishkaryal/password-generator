f8722f7a712b7f65a821137d25651d69f7ac58d5
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-11-03 19:33:00 +0545

    refactored header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,53 +2,53 @@ import React from "react";
 
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
-	ChakraComponent,
-	Heading,
-	HStack,
-	Icon,
-	IconButton,
-	Link,
-	useColorMode,
-	useColorModeValue,
+    ChakraComponent,
+    Heading,
+    HStack,
+    Icon,
+    IconButton,
+    Link,
+    useColorMode,
+    useColorModeValue,
 } from "@chakra-ui/react";
 
-import { FaCode } from "react-icons/fa";
-
-import { ICON_SIZE } from "@app/constants";
+import { FaGithub } from "react-icons/fa";
 
 export const Header: ChakraComponent<"div", {}> = () => {
-	const { toggleColorMode } = useColorMode();
-	return (
-		<HStack
-			id="header"
-			justifyContent="space-between"
-			paddingY={4}
-			paddingX={{ base: 4, md: 8 }}
-			boxShadow="lg"
-			width="100%"
-		>
-			<Heading as="h1" fontSize={{ base: "xl", md: "3xl" }} isTruncated>
-				Password Manager
-			</Heading>
-			<HStack spacing={4}>
-				<IconButton
-					aria-label="Switch between dark and light mode"
-					icon={useColorModeValue(
-						<MoonIcon boxSize={ICON_SIZE} />,
-						<SunIcon boxSize={ICON_SIZE} />
-					)}
-					backgroundColor="transparent"
-					onClick={toggleColorMode}
-					isRound
-				/>
-				<Link
-					isExternal
-					cursor="pointer"
-					href="https://github.com/aabishkaryal/password-manager"
-				>
-					<Icon as={FaCode} boxSize={ICON_SIZE} />
-				</Link>
-			</HStack>
-		</HStack>
-	);
+    const { toggleColorMode } = useColorMode();
+    return (
+        <HStack
+            id="header"
+            justifyContent="space-between"
+            paddingY={4}
+            paddingX={{ base: 4, md: 8 }}
+            boxShadow="lg"
+            width="100%"
+        >
+            <Heading as="h1" fontSize={{ base: "xl", md: "3xl" }} isTruncated>
+                Password Manager
+            </Heading>
+            <HStack spacing={2}>
+                <IconButton
+                    aria-label="Switch between dark and light mode"
+                    size="md"
+                    icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+                    backgroundColor="transparent"
+                    onClick={toggleColorMode}
+                    isRound
+                />
+
+                <IconButton
+                    aria-label="Link to Github repository."
+                    size="md"
+                    backgroundColor="transparent"
+                    icon={<Icon as={FaGithub} />}
+                    as={Link}
+                    href="https://github.com/aabishkaryal/password-manager"
+                    isRound
+                    isExternal
+                />
+            </HStack>
+        </HStack>
+    );
 };
